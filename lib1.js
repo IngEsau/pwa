2adafef8d62b74245be406922049fea59a979832
Daniel Esau <utp0157789@alumno.utpuebla.edu.mx>
2025-09-28 21:06:22 -0600

    (PWA3) add cacheTest

--- lib1.js
+++ lib1.js
@@ -7,21 +7,20 @@ class Quickchart {
     constructor(d){
         this.d = d;
     }
-    crearCadunos(){
-        let cadunos = "";
-        for(var i=0; i<this.d; i++){
-            cadunos += "1, "
-        }
-        cadunos = cadunos.slice(0,-1);
+    crearCadunos(){        
+        let cadunos = Array(Number(this.d)).fill("1").join(", ");
         return cadunos;
     }
-    generarSrcImg(){
+    generarSrcImg(){        
+        let etiquetas = [];
+        for(let i=0; i<this.d; i++){
+            etiquetas.push(i < n ? `1/${this.d}` : "");
+        }
         let url = "https://quickchart.io/chart?cht=p3&chd=t:" + this.crearCadunos()
-                    + "&chs=500x250&chl=" + "1/" + this.d;
+                    + "&chs=500x250&chl=" + etiquetas.join("|");
         return url;
     }
 }
-
 
 const q = new Quickchart(d)
 document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '" />';
--- sw.js
+++ sw.js
@@ -1,12 +1,37 @@
 self.addEventListener(
     'fetch',
-    function(evento){
+    function (evento) {
         console.log(evento.request.url);
-        if(/\.jpg$/.test(evento.request.url)){
+        if (/\.jpg$/.test(evento.request.url)) {
             evento.respondWith(fetch('unicorn.jpg'));
         }
-        else if(/\.png$/.test(evento.request.url)){
-            evento.respondWith(fetch('utp.png'));
+        else if (/\.png$/.test(evento.request.url)) {
+            evento.respondWith(fetch('utp.jpg'));
         }
     }
 )
+var cacheName = 'cacheTest';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll([
+                'sw.js',
+                'hola.jpg',
+                'index.html',
+                'lib1.js'
+            ]))
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
